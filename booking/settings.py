cb8cb1e846872cf15ad437bbb917ed50b66cc197
pharkusha <pharkusha@amplify.com>
2021-01-14 07:21:40 +0200

    add requirements

--- booking/settings.py
+++ booking/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -115,6 +115,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 PLACES_MAPS_API_KEY = 'AIzaSyBQL5NtkOVhW9AxuSIJUM64YAqVnMZ1uos'
 PLACES_MAP_WIDGET_HEIGHT = 480
