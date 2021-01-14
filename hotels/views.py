4144e074363a667f194c337190bdc8fe58d2e78d
pharkusha <pharkusha@amplify.com>
2021-01-14 14:15:50 +0200

    d

--- hotels/views.py
+++ hotels/views.py
@@ -4,14 +4,21 @@ from .models import Booking, Hotel
 from .serializer import BookingSerializer, HotelSerializer
 
 
+class BookingViewSet(viewsets.ReadOnlyModelViewSet):
+    serializer_class = HotelSerializer
+    queryset = Hotel.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(serializer_class=self.request.title)
+
+
 class HotelView(viewsets.ModelViewSet):
     serializer_class = BookingSerializer
     queryset = Booking.objects.all()
 
+    def perform_create(self, serializer):
+        serializer.save(serializer_class=self.request.title)
 
-class BookingViewSet(viewsets.ReadOnlyModelViewSet):
-    serializer_class = HotelSerializer
-    queryset = Hotel.objects.all()
 
 # class SingleHotelView(ListCreateAPIView):
 #     queryset = Booking.objects.all()
